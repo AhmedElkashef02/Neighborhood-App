03b7128783fbfd7d5b476c0e235aa4356340786f
AhmedElkashef02 <ahmedelkashef2012@gmail.com>
2018-01-31 19:38:54 +0200

    toggle sidebar using knockoutjs and data-bind

---
+++ js/mvvm.js
@@ -0,0 +1,14 @@
+var ViewModel = function() {
+
+  this.toggleClass = function(data, event) {
+    var wrapperClasses = document.getElementById("wrapper").classList;
+    if (wrapper.classList[0] == "toggled") {
+      wrapperClasses.remove("toggled");
+    } else {
+      wrapperClasses.add("toggled");
+    }
+  }
+
+}
+
+ko.applyBindings(new ViewModel())
