f22a8da535eca1169028006797a7a7474c63f296
Ahmed Salah Eldin Elkashef <ahmedelkashef2012@gmail.com>
2018-02-06 16:22:12 +0200

    document and clean code

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -25,7 +25,8 @@ function ViewModel() {
   this.searchTerm = ko.observable('');
   this.filteredLocations = ko.observableArray(locations);
 
-
+  // Filtering the results with the search term.
+  // This function hides the unneeded markers, and only displays filtered list
   this.filtration = function() {
     this.showListings();
     this.filteredLocations([]);
@@ -49,12 +50,12 @@ function ViewModel() {
     }
   }
 
-
+  //
   this.populateInfoWindow = function(marker, infowindow) {
   // Check to make sure the infowindow is not already opened on this marker.
   if (infowindow.marker != marker) {
     infowindow.marker = marker;
-    // Block of code to ajax call foursqaure API and get image
+    // Block of code to ajax call foursqaure API and get Info
     var title_generic = marker.title.split(" ")[0];
     var url = "https://api.foursquare.com/v2/venues/search?ll=" +
       marker.position.lat() + "," + marker.position.lng() + "&query=" + title_generic +
@@ -63,12 +64,14 @@ function ViewModel() {
       url: url,
       success: function(response) {
         var resposneObject = response.response.venues[0];
+        // Accomodating some not-always Available properties
         if(!resposneObject.location.address) {
           resposneObject.location.address = "Not Available";
         }
         if(!resposneObject.location.city) {
           resposneObject.location.city = "Giza";
         }
+        // setting content for markers
         infowindow.setContent('<div>' +
           '<h4>' + marker.title + '</h4>' +
           '<p><strong>Address:</strong>' + resposneObject.location.address + '</p>' +
@@ -90,6 +93,7 @@ function ViewModel() {
     }
   }
 
+  // Opens the same marker that was clicked on from the list
   this.openFromList = function(event) {
     for (var i = 0; i < self.markers.length; i++) {
       if (self.markers[i].title == event.title) {
@@ -104,6 +108,7 @@ function ViewModel() {
     }).bind(this), 700);
   };
 
+  // Shows all markers on the load of the map
   this.showListings = function() {
     this.bounds = new google.maps.LatLngBounds();
     // Extend the boundaries of the map for each marker and display the marker
@@ -114,8 +119,8 @@ function ViewModel() {
     map.fitBounds(this.bounds);
   };
 
+  // Hides all markers
   this.hideListings = function() {
-    //this.showListings();
     for (var i = 0; i < this.markers.length; i++) {
       for (var j = 0; j < undesiredLocations.length; j++) {
         if (self.markers[i].title == undesiredLocations[j].title) {
@@ -125,6 +130,7 @@ function ViewModel() {
     }
   };
 
+  // Initializing the map
   this.initMap = function() {
     // Create an InfoWindow
     this.largeInfoWindow = new google.maps.InfoWindow();
@@ -159,9 +165,9 @@ function ViewModel() {
 
     this.showListings();
   };
-
   this.initMap();
 
+  // Toggles the sidebar on front-end
   this.toggleClass = function(data, event) {
     var wrapperClasses = document.getElementById("wrapper").classList;
     if (wrapper.classList[0] == "toggled") {
@@ -173,6 +179,7 @@ function ViewModel() {
 
 }
 
+// applyBindings and fire the app
 function fireApp() {
   ko.applyBindings(new ViewModel());
 };
